3804c9566217431e805f98795301495bcd42a450
Frank <largewowfan@gmail.com>
2017-04-05 12:53:13 +0200

    - Added API_Uri
    - Added Empty solution

---
+++ src/OpenDataApplication/OpenDataApplication/MainForm.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenDataApplication
+{
+    using System.Windows.Forms;
+
+    public sealed partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
