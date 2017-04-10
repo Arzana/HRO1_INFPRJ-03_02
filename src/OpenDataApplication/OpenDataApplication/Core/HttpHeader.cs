c5b5d26b5119900f5df0737dcb8d800ad75e27aa
Eric-Laptop\Eric <duck11@upcmail.nl>
2017-04-10 10:46:33 +0100

    Added HTTPwebrequest returns XMLfile

---
+++ src/OpenDataApplication/OpenDataApplication/Core/HttpHeader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.Console;
+using static System.ConsoleKey;
+
+
+namespace OpenDataApplication.Core
+{
+    class HttpHeader
+    {
+        const string URL = "https://webservices.ns.nl/ns-api-avt?station=ut";
+        const string username = "0916827@hr.nl";
+        const string password = "6qmg-XC7AH61Wz53i89ZC-bVSyab7QYTD6nS_Dx6wlLoMM_cFzzSXA";
+
+
+        public static XmlDocument webRequest()
+        {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = WebRequestMethods.Http.Get;
+            request.Credentials = new NetworkCredential(username, password);
+            request.ContentType = "text/xml; encoding='utf-8'";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            XmlDocument xmlDocu = new XmlDocument();
+
+            XmlTextReader reader = new XmlTextReader(response.GetResponseStream());
+            xmlDocu.Load(reader);
+
+            return(xmlDocu);
+        }
+    }
+}
